417dd5cabee3483777663c49bbf2a6db5de6fef8
arduino_explorer <42172864+arj101@users.noreply.github.com>
2022-09-08 16:25:23 +0530

    fix errors

--- supplier-bot-base.ino
+++ supplier-bot-base.ino
@@ -42,9 +42,6 @@ volatile unsigned long timerStart1, timerStart2, pulseDuration1, pulseDuration2;
 
 char movementState = 's';
 
-int calcDistance(int pulseDuration) {
-  return pulseDuration * 0.034 / 2;
-}
 
 void isr1() {
   echoState1 = digitalRead(echoPin1);
@@ -82,7 +79,7 @@ void setup() {
   pinMode(echoPin2, INPUT);
   
 
-   attachInterrupt(digitalPinToInterrupt(echoPin1), isr1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(echoPin1), isr1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(echoPin2), isr2, CHANGE);
 }
 void setMotorPins(bool mLp1, bool mLp2, bool mRp1, bool mRp2) {
@@ -101,18 +98,10 @@ void moveA(char dir, bool fwd = true) {
     case 'r': setMotorPins(fwd, !fwd, !fwd, !fwd); break;
     case 'l': setMotorPins(!fwd, !fwd, fwd, !fwd); break;
     case 's': setMotorPins(LOW, LOW, LOW, LOW);
-     
   }
 }
 
 
-
-//bool obstacleAhead() {
-//  int d1 = ulSensor1.pulseDuration * 0.034 / 2;
-//  int d2 = ulSensor2.pulseDuration * 0.034 / 2;
-//
-//  return d1 < 20 || d2 < 20;
-//}
 
 void kitchenMode() {
   if (sensorVal[3] && sensorVal[2]) {
@@ -157,26 +146,15 @@ void tableMode() {
 void moveManual(char dir) {
   movementState = dir;
   switch (dir) {
-    case FORWARD_C: if(!collissionDetected) setMotorPins(HIGH, LOW, HIGH, LOW);
-      mode = '\0';
-      break;
-    case BACKWARD_C: setMotorPins(LOW, HIGH, LOW, HIGH);
-    mode = '\0';
-    break;
-
-    case LEFT_C: setMotorPins(LOW, LOW, HIGH, LOW);
-    mode = '\0';
-    break;
-    case RIGHT_C: setMotorPins(HIGH, LOW, LOW, LOW);
-    mode = '\0';break;
-
+    case FORWARD_C: if(!collissionDetected) setMotorPins(HIGH, LOW, HIGH, LOW);break;
+    case BACKWARD_C: setMotorPins(LOW, HIGH, LOW, HIGH);break;
+    case LEFT_C: setMotorPins(LOW, LOW, HIGH, LOW);break;
+    case RIGHT_C: setMotorPins(HIGH, LOW, LOW, LOW);break;
     case STOP_C: setMotorPins(LOW, LOW, LOW, LOW);
-    mode = '\0';
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     char c = Serial.read();
     moveManual(c);
@@ -191,29 +169,15 @@ void loop() {
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   delay(50);
 
 
-//  if (!collissionDetected) {
-    if (mode == 'k') kitchenMode();
-    else if (mode == 't') tableMode();
-//  } else if (movementState == 'f' || (movementState != 's' && (mode == 't' || mode == 'k'))) {
-//    setMotorPins(LOW, LOW, LOW, LOW);
-//
-//    if(lastCollissionMsgTime == 0 || millis() - lastCollissionMsgTime > 2000) {
-//      Serial.print('O');
-//      lastCollissionMsgTime = millis();
-//    }
- // }
+
+  if (mode == 'k') kitchenMode();
+  else if (mode == 't') tableMode();
 
   for(int i = 0; i < 4; i++) prevSensorVal[i] = sensorVal[i];
-
-//  if (millis() - lastCollissionCheck > 200) {
-//      collissionDetected = calcDistance(pulseDuration1) <= 20 || calcDistance(pulseDuration2) <= 20;
-//      lastCollissionCheck = millis();
-//  }
 }
